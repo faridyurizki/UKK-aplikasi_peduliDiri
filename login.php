6e8489f7196ed0593881e898f6c74e08faa5750e
faridyurizki <faridyourizki@gmail.com>
2023-01-16 15:43:51 +0700

    first form

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+    <form action="">
+        <table align="center" >
+            <tr>
+                <td><input type="text" name="nik" placeholder="NIK" ></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name" placeholder="nama"></td>
+            </tr>
+            <tr>
+                <input type="button" name="signup" value="belum punya akun">
+                <input type="button" name="masuk" value="masuk">
+            </tr>
+        </table>
+    </form>
+
+</html>
