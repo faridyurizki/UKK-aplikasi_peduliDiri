a8e46087bb809437e79aaffdb6906679323b22ec
faridyurizki <faridyourizki@gmail.com>
2023-01-25 18:36:45 +0700

    test

--- login.php
+++ login.php
@@ -1,15 +1,39 @@
+<?php
+    if (isset($_POST['daftar'])){
+        $nik = $_POST['nik'];
+        $name = $_POST['name'];
+        $text = $nik.",".$name."\n";
+       
+            if($nik == '' and $name == ''){
+                echo "<script>alert ('data tidak boleh kosong') </script>";
+            } else {
+                $fp = fopen('config.txt', 'a+');
+                
+                if(fwrite($fp, $text)){
+                    echo "<script>alert ('anda berhasil mendaftar') </script>";
+                }
+        
+            }
+
+
+    }
+    ?>
+
 <html>
-    <form action="">
+    <form action="" method="post">
+    <br><br><br><br><br><br><br><br><br><br><br><br>
         <table align="center" >
             <tr>
-                <td><input type="text" name="nik" placeholder="NIK" ></td>
+                <td><input type="text" name="nik" placeholder="NIK" required ></td>
             </tr>
             <tr>
-                <td><input type="text" name="name" placeholder="nama"></td>
+                <td><input type="text" name="name" placeholder="nama lengkap" required></td>
             </tr>
             <tr>
-                <input type="button" name="signup" value="belum punya akun">
-                <input type="button" name="masuk" value="masuk">
+                <td>
+                <input type="submit" name="daftar" value="saya belum punya akun">
+                <input type="submit" name="masuk" value="masuk">
+                </td>
             </tr>
         </table>
     </form>
